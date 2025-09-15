c70d3b8b694b373ded430174abf86fb0ebfe10a5
Hatsune Miku <l2a1knla@gmail.com>
2025-09-15 18:31:13 +0800

    first

--- main.js
+++ main.js
@@ -7,20 +7,23 @@ const app = express()
 app.use((req, res, next) => {
   if (req.headers['content-type']?.includes('application/json')) {
     bodyParser.json()(req, res, next)
+  } else if (
+    req.headers['content-type']?.includes('application/x-www-form-urlencoded')
+  ) {
+    bodyParser.urlencoded({ extended: true })(req, res, next)
   } else {
     next()
   }
 })
 
-app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.raw())
 const proxy = createProxyMiddleware({
   target: 'https://www.kookapp.cn',
   changeOrigin: true,
   ws: true,
-  onProxyReq: (proxyReq, req) => {
-    proxyReq.write(req.body)
-  },
+  //   onProxyReq: (proxyReq, req) => {
+  //     proxyReq.write(req.body)
+  //   },
 })
 
 app.use('/api/v3/aaa/bbb', (req, res, next) => {
