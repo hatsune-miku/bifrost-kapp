cf79756cd426a21d5acbddb38a5ace3e19e5bb73
Hatsune Miku <l2a1knla@gmail.com>
2025-09-30 19:48:39 +0800

    first

--- src/main.ts
+++ src/main.ts
@@ -26,13 +26,13 @@ const proxy = createProxyMiddleware({
   },
 })
 
-const staticProxy = createProxyMiddleware({
-  target: 'https://static.kookapp.cn',
+const assetsProxy = createProxyMiddleware({
+  target: 'https://img.kookapp.cn',
   changeOrigin: true,
   on: {
     proxyReq: (proxyReq, req, res) => {
       const headers = new Map()
-      const obj = {
+      Object.entries({
         'cache-control': 'no-cache',
         'sec-ch-ua': '"Chromium";v="140", "Not=A?Brand";v="24", "Google Chrome";v="140"',
         'sec-ch-ua-mobile': '?0',
@@ -49,32 +49,41 @@ const staticProxy = createProxyMiddleware({
         'accept-encoding': 'gzip, deflate, br, zstd',
         'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8,zh-CN;q=0.7,zh;q=0.6',
         referer: 'https://www.kookapp.cn',
-      }
-      Object.entries(obj).forEach(([key, value]) => {
+      }).forEach(([key, value]) => {
         headers.set(key, value)
       })
       proxyReq.setHeaders(headers)
-      // req.headers['authority'] = 'static.kookapp.cn'
-      // req.headers['host'] = 'www.kookapp.cn'
-      // req.headers['referer'] = 'https://www.kookapp.cn'
-      // req.headers['sec-ch-ua'] = '"Chromium";v="140", "Not=A?Brand";v="24", "Google Chrome";v="140"'
-      // req.headers['sec-ch-ua-mobile'] = '?0'
-      // req.headers['sec-ch-ua-platform'] = '"Windows"'
-      // req.headers['user-agent'] =
-      //   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36'
-      // req.headers['accept'] = '*/*'
-      // req.headers['accept-encoding'] = 'gzip, deflate, br, zstd'
-      // req.headers['accept-language'] = 'en-GB,en-US;q=0.9,en;q=0.8,zh-CN;q=0.7,zh;q=0.6'
-      // req.headers['cache-control'] = 'no-cache'
-      // req.headers['pragma'] = 'no-cache'
-      // req.headers['priority'] = 'u=2'
-      // req.headers['sec-fetch-dest'] = 'script'
-      // req.headers['sec-fetch-mode'] = 'no-cors'
-      // req.headers['sec-fetch-site'] = 'same-site'
-      console.log('xx', 'e', req.headers)
     },
-    proxyRes: (proxyRes, req, res) => {
-      console.log('xx', 'proxyRes', proxyRes.statusCode, proxyRes.headers)
+  },
+})
+
+const staticProxy = createProxyMiddleware({
+  target: 'https://static.kookapp.cn',
+  changeOrigin: true,
+  on: {
+    proxyReq: (proxyReq, req, res) => {
+      const headers = new Map()
+      Object.entries({
+        'cache-control': 'no-cache',
+        'sec-ch-ua': '"Chromium";v="140", "Not=A?Brand";v="24", "Google Chrome";v="140"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'upgrade-insecure-requests': '1',
+        'user-agent':
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36',
+        accept:
+          'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'sec-fetch-site': 'none',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-user': '?1',
+        'sec-fetch-dest': 'document',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8,zh-CN;q=0.7,zh;q=0.6',
+        referer: 'https://www.kookapp.cn',
+      }).forEach(([key, value]) => {
+        headers.set(key, value)
+      })
+      proxyReq.setHeaders(headers)
     },
   },
 })
