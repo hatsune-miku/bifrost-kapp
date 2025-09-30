f23e1636e26ce1a6d058b0448c2c08239ba7f29f
Hatsune Miku <l2a1knla@gmail.com>
2025-09-30 19:42:59 +0800

    first

--- src/main.ts
+++ src/main.ts
@@ -31,6 +31,26 @@ const staticProxy = createProxyMiddleware({
   changeOrigin: true,
   on: {
     proxyReq: (proxyReq, req, res) => {
+      req.headers = {
+        host: 'localhost:9872',
+        connection: 'close',
+        pragma: 'no-cache',
+        'cache-control': 'no-cache',
+        'sec-ch-ua': '"Chromium";v="140", "Not=A?Brand";v="24", "Google Chrome";v="140"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'upgrade-insecure-requests': '1',
+        'user-agent':
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36',
+        accept:
+          'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'sec-fetch-site': 'none',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-user': '?1',
+        'sec-fetch-dest': 'document',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8,zh-CN;q=0.7,zh;q=0.6',
+      }
       // req.headers['authority'] = 'static.kookapp.cn'
       // req.headers['host'] = 'www.kookapp.cn'
       // req.headers['referer'] = 'https://www.kookapp.cn'
